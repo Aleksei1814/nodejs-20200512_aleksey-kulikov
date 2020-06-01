320b3cd242cf4d32d5ea3b9c5f656c3a87b53770
alexei.kulikov <alexei.kulikov@surasystems.com>
2020-06-01 07:09:28 +0300

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,27 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.setEncoding(options.encoding || 'utf-8');
+    this.chunkDataString = null;
   }
 
   _transform(chunk, encoding, callback) {
+    let chunkData = chunk.toString();
+    if (this.chunkDataString) {
+      chunkData = this.chunkDataString + chunkData;
+    }
+    const lines = chunkData.split(os.EOL);
+    this.chunkDataString = lines.splice(lines.length-1, 1)[0];
+    for (const item of lines) {
+      this.push(item);
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.chunkDataString) this.push(this.chunkDataString);
+    this.chunkDataString = null;
+    callback();
   }
 }
 
