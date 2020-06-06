4d723a31e33a705214a758fe101e87441d5a5c46
alexei.kulikov <alexei.kulikov@surasystems.com>
2020-06-07 01:31:26 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,27 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      fs.createReadStream(filepath)
+          .on('error', (error) => {
+            switch (error.code) {
+              case 'ENOENT':
+                if (pathname.split('/').length > 1) {
+                  res.statusCode = 400;
+                  res.end('subfolders are not supported');
+                } else {
+                  res.statusCode = 404;
+                  res.end('No such file or directory');
+                }
+                break;
+              case 'EISDIR':
+                res.statusCode = 400;
+                res.end('Is a directory');
+                break;
+              default:
+                res.statusCode = 500;
+                res.end('internal server error');
+            }
+          }).pipe(res);
 
       break;
 
