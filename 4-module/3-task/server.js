4e9538510e33981dfbeb113a12db831d63c26421
alexei.kulikov <alexei.kulikov@surasystems.com>
2020-06-07 01:52:17 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,36 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      fs.stat(filepath, (error) => {
+        if (error) {
+          switch (error.code) {
+            case 'ENOENT':
+              if (pathname.split('/').length > 1) {
+                res.statusCode = 400;
+                res.end('subfolders are not supported');
+              } else {
+                res.statusCode = 404;
+                res.end('No such file');
+              }
+              break;
+            default:
+              res.statusCode = 500;
+              res.end('something wrong');
+          }
+          return;
+        }
+
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end('something wrong');
+            return;
+          }
+          res.statusCode = 200;
+          res.end('its ok');
+        });
+      });
+
 
       break;
 
